76a58ee0fd3a67b621b70eb5b8dc31f6af293f73
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-28 11:26:38 +0300

    Update SubsetOfSquaresCreator.java

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -5,5 +5,14 @@ import java.util.Set;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+      
+        NavigableSet<Integer> navigableSet = new TreeSet<>();
+        for (Integer number : sourceList) {
+            int square = number * number;
+            if ((square >= lowerBound) && (square <= upperBound)){
+                navigableSet.add(square);
+            }
+        }
+        return navigableSet;
     }
 }
