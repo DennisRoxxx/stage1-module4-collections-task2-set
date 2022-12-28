142084595df89b447264306405adf60b5a07b12c
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-28 13:53:34 +0300

    Update SubsetOfSquaresCreator.java

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,7 +1,9 @@
 package com.epam.mjc.collections.set;
 
 import java.util.List;
+import java.util.NavigableSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
